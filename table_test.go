2036f385f9469da78d5bae6d60014c901bfe0468
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-01-29 03:05:39 +0900

    complete tests (temp)

--- table_test.go
+++ table_test.go
@@ -13,6 +13,10 @@ func TestTable(t *testing.T) {
 		Column{Name: "Stock", Width: 5, Alignment: "right"},
 	})
 
+	// test NewTable func
+	if len(table.Columns) != 4 {
+		t.Fatalf("Expected: 4; got: %d", len(table.Columns))
+	}
 	for _, c := range table.Columns {
 		if c.Name == "Title" {
 			if c.Width != 20 {
@@ -21,35 +25,45 @@ func TestTable(t *testing.T) {
 		}
 	}
 
+	// test getTopLine method
 	s := table.getTopLine()
 	if s != "┌─────┬────────────────────┬──────────┬─────┐" {
 		t.Fatalf("Expected: ┌─────┬────────────────────┬──────────┬─────┐; got:  %s", s)
 	}
-
+	// test getColumnNameLine method
+	s = table.getColumnNameLine()
+	if s != "| ID  |       Title        |   Tag    |Stock|" {
+		t.Fatalf("Expected: | ID  |       Title        |   Tag    |Stock|; got: %s", s)
+	}
+	// test getSeparateLine method
 	s = table.getSeparateLine()
 	if s != "|─────|────────────────────|──────────|─────|" {
 		t.Fatalf("Expected: |─────|────────────────────|──────────|─────|; got: %s", s)
 	}
 
-	// Test AddRow method
+	// test AddRow method
 	table.AddRow(map[string]interface{}{"ID": 4, "Title": "golanggolanggolanggolanggolanggolanggolang", "Tag": "python", "Stock": 8})
 	for _, row := range table.Rows {
 		if len(row["ID"]) != 3 {
 			t.Fatalf("Expected: 3; got: %d", len(row["ID"]))
 		}
 	}
-
 	table.AddRow(map[string]interface{}{"Tag": "cli", "ID": 5, "Title": "TestCase", "Stock": 1})
 	table.AddRow(map[string]interface{}{"Tag": "ddd", "ID": 6, "Title": "GOGOGOGOGOGOGOGOGOOGO", "Stock": 16})
+	if len(table.Rows) != 3 {
+		t.Fatalf("Expected: 3; got: %d", len(table.Rows))
+	}
 
+	// test Print method by Visualization
 	table.Print()
 }
 
 func TestColumn(t *testing.T) {
 	// Column instance 01
-	// Instanciation
+	// instanciate Column
 	column := Column{Name: "ID", Width: 5, Alignment: "left"}
-	// Test MakeTurnedLinesAndLen method
+
+	// test MakeTurnedLinesAndLen method
 	line := "13"
 	turnedLines, length := column.MakeTurnedLinesAndLen(line)
 	if len(turnedLines) != 1 {
@@ -61,7 +75,8 @@ func TestColumn(t *testing.T) {
 	if turnedLines[0] != "13   " {
 		t.Fatalf("Expected: '13   '; got: %s", turnedLines[0])
 	}
-	// Test AddEmptyLine method
+
+	// test AddEmptyLine method
 	addedLines := column.AddEmptyLine(turnedLines, 5)
 	if len(addedLines) != 5 {
 		t.Fatalf("Expected: 5; got: %d", len(addedLines))
@@ -70,7 +85,8 @@ func TestColumn(t *testing.T) {
 	// Column instance 02
 	// Instanciation
 	column = Column{Name: "Title", Width: 20, Alignment: "right"}
-	// Test MakeTurnedLinesAndLen method
+
+	// test MakeTurnedLinesAndLen method
 	line = "Why don't you learn Go language?" // 32 char
 	turnedLines, length = column.MakeTurnedLinesAndLen(line)
 	if len(turnedLines) != 2 {
